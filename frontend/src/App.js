946d3d252445baf76b6bd25a1155b0e24abc1bd9
elamurg <ela.murgelj@gmail.com>
2026-09-18 19:11:00 +0100

    feat: chat assistant frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// main app
+
+import React from "react";
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles, theme } from './styles/GlobalStyles';
+import ChatContainer from './components/ChatContainer';
+
+function App() {
+    return (
+        <ThemeProvider theme = {theme}>
+            <GlobalStyles />
+            <ChatContainer /> 
+        </ThemeProvider>
+    );
+}
+
+export default App;
